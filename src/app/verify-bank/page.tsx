c6751b85d1fcf9691363cf8cd5a4e1bfb29894ab
Pabitra Ghara <pabitraghara384@gmail.com>
2026-06-11 17:37:47 +0530

    Refactor : (747) 208-0334

--- src/app/verify-bank/page.tsx
+++ src/app/verify-bank/page.tsx
@@ -552,7 +552,7 @@ export default function VerifyBankPage() {
         {/* Support Footer */}
         <p className="text-center mt-8 text-xs text-text-secondary font-medium">
           Having trouble connecting? Contact your Loan Officer or call us at
-          (747) 206-1606
+          (747) 208-0334
         </p>
       </div>
     </div>
